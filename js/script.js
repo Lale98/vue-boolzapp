c6e1c7b1fbff7bd17d41ad71e1008dffbe6cc631
Lale98 <78683721+Lale98@users.noreply.github.com>
2021-05-26 01:11:34 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -10,39 +10,51 @@ var app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        info: false,
+                        delete: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'sent',
+                        info: false,
+                        delete: false
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        info: false,
+                        delete: false
                     }
                 ],
             },
             {
                 name: 'Fabio',
                 avatar: '_2',
-                visible: true,
+                visible: false,
                 messages: [
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        info: false,
+                        delete: false
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        info: false,
+                        delete: false
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        info: false,
+                        delete: false
                     }
                 ],
             },     
@@ -54,17 +66,23 @@ var app = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        info: false,
+                        delete: false
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        info: false,
+                        delete: false
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        info: false,
+                        delete: false
                     }
                 ],
             },
@@ -76,12 +94,16 @@ var app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        info: false,
+                        delete: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        info: false,
+                        delete: false
                     }
                 ],
             }
@@ -89,7 +111,9 @@ var app = new Vue({
         indexActive: 0,
         mess: "",
         chatName: "",
-        colorMode: false
+        colorMode: false,
+        detail: false,
+        messageIndex: 0
     },
     methods: {
         addImg: function(index) {
@@ -109,13 +133,17 @@ var app = new Vue({
                 this.contacts[this.indexActive].messages.push({
                     date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
                     text: this.mess,
-                    status: 'sent'});
+                    status: 'sent',
+                    info: false,
+                    delete: false});
                 this.mess = "";
                 setTimeout(() => {
                     this.contacts[this.indexActive].messages.push({
                         date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
                         text: "Ok",
-                        status: 'received'});
+                        status: 'received',
+                        info: false,
+                        delete: false});
                     }, 1000);
             };
         },
@@ -130,6 +158,12 @@ var app = new Vue({
         },
         notifyOn: function() {
             alert('Non ti scrive nessuno, a cosa ti servono le notifiche ?');
+        },
+        info: function(index) {
+            this.contacts[this.indexActive].messages[index].info = !this.contacts[this.indexActive].messages[index].info;
+        },
+        del: function(index) {
+            this.contacts[this.indexActive].messages[index].delete = true;
         }
     },
     updated: function () {
